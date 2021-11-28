efa31c49330f09e99b4ed5826689e2399163a6f7
Disengager <disengagerAMV@gmail.com>
2021-11-29 00:21:57 +0300

    тестовая версия

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ const study = lab.util.fromObject({
     },
     {
       "type": "lab.plugins.Download",
-      "filePrefix": "study",
+      "filePrefix": "replikaciya-dashi-kostinoj",
       "path": undefined
     }
   ],
   "metadata": {
-    "title": "",
+    "title": "Репликация Даши Костиной",
     "description": "",
     "repository": "",
     "contributors": ""
@@ -29,7 +29,7 @@ const study = lab.util.fromObject({
         {
           "required": true,
           "type": "html",
-          "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E\r\n    Здравствуйте, меня зовут Константин. Пожалуйста пройдите тест.\r\n    \u003Cbr\u003E\u003Cbr\u003E\r\n    Во время эксперимента вам необходимо нажимать клавишу \u003Cspan class=\"key-button\"\u003Eq\u003C\u002Fspan\u003E, если цвет несоотвествуют тексту и \u003Cspan class=\"key-button\"\u003Ew\u003C\u002Fspan\u003E, если соотвествует.\r\n    \u003Cbr\u003E\u003Cbr\u003E   \r\n    Нажимите \u003Cspan class=\"key-button\"\u003Eпробел\u003C\u002Fspan\u003E чтобы продоложить. \r\n  \u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
+          "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E\r\n    Здравствуйте, меня зовут Константин. \u003Cbr\u003E \r\n    Спасибо, что решили поучаствовать в моём эксперименте.\r\n    \u003Cbr\u003E\u003Cbr\u003E   \r\n    Нажимите \u003Cspan class=\"key-button\"\u003Eпробел\u003C\u002Fspan\u003E чтобы продоложить. \r\n  \u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
           "name": ""
         }
       ],
@@ -67,8 +67,8 @@ const study = lab.util.fromObject({
         {
           "required": true,
           "type": "input",
-          "label": "Код для прохождения эксперимента",
-          "name": "IC"
+          "label": "Ваше образование",
+          "name": "education"
         }
       ],
       "scrollTop": true,
@@ -79,23 +79,258 @@ const study = lab.util.fromObject({
         "": ""
       },
       "parameters": {},
-      "messageHandlers": {},
+      "messageHandlers": {
+        "end": function anonymous(
+) {
+this.options.parameters.hallo = 'hallo';
+window.biograph_data = this.data;
+
+}
+      },
       "title": "biography"
     },
     {
       "type": "lab.flow.Loop",
       "templateParameters": [
         {
-          "word": "красный",
-          "color": "red"
+          "prime_task": "8 + 4",
+          "target_task": "12 + 25",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 1,
+          "name": "prime1"
         },
         {
-          "word": "синий",
-          "color": "red"
+          "prime_task": "6 + 2",
+          "target_task": "17 + 12",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 3,
+          "name": "prime2"
         },
         {
-          "word": "фиолетовый",
-          "color": "purple"
+          "prime_task": "7 + 3",
+          "target_task": "19 + 27",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 2,
+          "name": "prime3"
+        },
+        {
+          "prime_task": "3 + 2",
+          "target_task": "15 + 16",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 3,
+          "name": "prime4"
+        },
+        {
+          "prime_task": "9 + 2",
+          "target_task": "30 + 22",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 9,
+          "counterbalancing": 1,
+          "name": "prime5"
+        },
+        {
+          "prime_task": "5 + 4",
+          "target_task": "22 + 51",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 2,
+          "name": "prime6"
+        },
+        {
+          "prime_task": "7 + 4",
+          "target_task": "20 + 48",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 3,
+          "counterbalancing": 2,
+          "name": "prime7"
+        },
+        {
+          "prime_task": "5 + 2",
+          "target_task": "39 + 23",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 1,
+          "name": "prime8"
+        },
+        {
+          "prime_task": "9 + 6",
+          "target_task": "32 + 50",
+          "prime_task_answer": 15,
+          "prime_task_wronganser": 18,
+          "counterbalancing": 3,
+          "name": "prime9"
+        },
+        {
+          "prime_task": "8 + 9",
+          "target_task": "59 + 34",
+          "prime_task_answer": 17,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 1,
+          "name": "prime10"
+        },
+        {
+          "prime_task": "9 + 5",
+          "target_task": "16 + 50",
+          "prime_task_answer": 14,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 2,
+          "name": "prime11"
+        },
+        {
+          "prime_task": "9 + 2",
+          "target_task": "39 + 48",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 12,
+          "counterbalancing": 3,
+          "name": "prime12"
+        },
+        {
+          "prime_task": "8 + 7",
+          "target_task": "29 + 40",
+          "prime_task_answer": 15,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 3,
+          "name": "prime13"
+        },
+        {
+          "prime_task": "4 + 5",
+          "target_task": "36 + 58",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 2,
+          "name": "prime14"
+        },
+        {
+          "prime_task": "4 + 1",
+          "target_task": "54 + 38",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 1,
+          "name": "prime15"
+        },
+        {
+          "prime_task": "2 + 6",
+          "target_task": "22 + 76",
+          "prime_task_answer": 8,
+          "prime_task_wronganser": 9,
+          "counterbalancing": 2,
+          "name": "prime16"
+        },
+        {
+          "prime_task": "8 + 9",
+          "target_task": "51 + 43",
+          "prime_task_answer": 17,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 1,
+          "name": "prime17"
+        },
+        {
+          "prime_task": "4 + 2",
+          "target_task": "53 + 19",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 3,
+          "name": "prime18"
+        },
+        {
+          "prime_task": "3 + 4",
+          "target_task": "28 + 22",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime19"
+        },
+        {
+          "prime_task": "2 + 4",
+          "target_task": "15 + 58",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 9,
+          "counterbalancing": 1,
+          "name": "prime20"
+        },
+        {
+          "prime_task": "1 + 3",
+          "target_task": "25 + 53",
+          "prime_task_answer": 4,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 3,
+          "name": "prime21"
+        },
+        {
+          "prime_task": "5 + 3",
+          "target_task": "44 + 38",
+          "prime_task_answer": 8,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime22"
+        },
+        {
+          "prime_task": "8 + 5",
+          "target_task": "11 + 27",
+          "prime_task_answer": 13,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 3,
+          "name": "prime23"
+        },
+        {
+          "prime_task": "6 + 6",
+          "target_task": "50 + 29",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 1,
+          "name": "prime24"
+        },
+        {
+          "prime_task": "2 + 3",
+          "target_task": "22 + 16",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 2,
+          "counterbalancing": 1,
+          "name": "prime25"
+        },
+        {
+          "prime_task": "7 + 6",
+          "target_task": "56 + 29",
+          "prime_task_answer": 13,
+          "prime_task_wronganser": 18,
+          "counterbalancing": 2,
+          "name": "prime26"
+        },
+        {
+          "prime_task": "6 + 3",
+          "target_task": "26 + 72",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 3,
+          "name": "prime27"
+        },
+        {
+          "prime_task": "8 + 2",
+          "target_task": "67 + 19",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 1,
+          "name": "prime28"
+        },
+        {
+          "prime_task": "7 + 4",
+          "target_task": "27 + 35",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 10,
+          "counterbalancing": 3,
+          "name": "prime29"
+        },
+        {
+          "prime_task": "7 + 8",
+          "target_task": "48 + 25",
+          "prime_task_answer": 15,
+          "prime_task_wronganser": 19,
+          "counterbalancing": 2,
+          "name": "prime30"
         }
       ],
       "sample": {
@@ -106,9 +341,59 @@ const study = lab.util.fromObject({
         "": ""
       },
       "parameters": {},
-      "messageHandlers": {},
-      "title": "stroop_loop",
-      "indexParameter": "counter",
+      "messageHandlers": {
+        "before:prepare": function anonymous(
+) {
+window.params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    .reduce(
+        function(p,e){
+            var a = e.split('=');
+            p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+            return p;
+        },
+        {}
+    );
+
+window.counterbalancing_list = {}
+
+console.log(window.params['key'])
+var xmlhttp = new XMLHttpRequest(),
+    sliceFormData = new FormData(),
+    theUrl = "https://experiments-server.herokuapp.com/key/";
+sliceFormData.append("key", JSON.stringify(window.params['key']));
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      if(xmlhttp.responseText == 'NO USER') {
+        window.params['key'] = 'NO USER'
+      } else {
+        let data = JSON.parse(xmlhttp.responseText)
+        window.counterbalancing_list = data
+      }
+    }
+}
+xmlhttp.open("POST", theUrl, false);
+xmlhttp.send(sliceFormData);
+
+let params = this.internals.parsedOptions.templateParameters
+if (Object.keys(window.counterbalancing_list).length === 0) {
+  for (var i in params) {
+    let index = parseInt(i) + parseInt(1)
+    console.log('prime' + index)
+    window.counterbalancing_list['prime' + index] = params[i].counterbalancing
+  }
+}
+
+
+console.log(window.counterbalancing_list)
+
+
+}
+      },
+      "title": "arithmetic_loop",
       "shuffleGroups": [],
       "template": {
         "type": "lab.flow.Sequence",
@@ -126,7 +411,7 @@ const study = lab.util.fromObject({
               {
                 "required": true,
                 "type": "html",
-                "content": "\u003Ccenter\u003E\u003Ch3\u003E+\u003C\u002Fh3\u003E\u003C\u002Fcenter\u003E",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E&&&&&&&&&&\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_and top\"\u003E&&&&&&&&&&\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_and bottom\"\u003E&&&&&&&&&&\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
                 "name": ""
               }
             ],
@@ -139,7 +424,56 @@ const study = lab.util.fromObject({
             },
             "parameters": {},
             "messageHandlers": {},
-            "title": "cross",
+            "title": "start_mask",
+            "timeout": "200"
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E \r\n    ${ window.counterbalancing_list[parameters.name] != 3 ? parameters.prime_task : \"xxxxxx\" }\r\n  \u003Ch2\u003E\r\n\u003C\u002Fcenter\u003E",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Continue →",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": ""
+            },
+            "parameters": {},
+            "messageHandlers": {
+              "run": function anonymous(
+) {
+//alert(window.biograph_data.group_choice)
+}
+            },
+            "title": "prime_task",
+            "timeout": "17"
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_x top\"\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_x bottom\"\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E ",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Continue →",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": ""
+            },
+            "parameters": {},
+            "messageHandlers": {},
+            "title": "middle_mask",
             "timeout": "500"
           },
           {
@@ -148,28 +482,8 @@ const study = lab.util.fromObject({
               {
                 "required": true,
                 "type": "html",
-                "content": "\u003Ccenter\u003E\r\n  \u003Ch2 style=\"color: ${ parameters.color }\"\u003E${ parameters.word }\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E${ parameters.target_task }\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
                 "name": ""
-              }
-            ],
-            "scrollTop": true,
-            "submitButtonText": "Continue →",
-            "submitButtonPosition": "hidden",
-            "files": {},
-            "responses": {
-              "keypress(q, й)": "нет",
-              "keypress(w, ц)": "да"
-            },
-            "parameters": {},
-            "messageHandlers": {},
-            "title": "stroop_page",
-            "plugins": []
-          },
-          {
-            "type": "lab.html.Page",
-            "items": [
-              {
-                "type": "text"
               }
             ],
             "scrollTop": true,
@@ -181,7 +495,115 @@ const study = lab.util.fromObject({
             },
             "parameters": {},
             "messageHandlers": {},
-            "title": "interval",
+            "title": "target_task",
+            "timeout": "400"
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E\r\n    ${ window.counterbalancing_list[parameters.name] != 3 ? (\r\n      window.counterbalancing_list[parameters.name] == 1 ? (parameters.prime_task + ' = ' + parameters.prime_task_answer) : (parameters.prime_task + ' = ' + parameters.prime_task_wronganser)\r\n      ) : \"xxxxxx\" } \r\n  \u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Continue →",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": ""
+            },
+            "parameters": {},
+            "messageHandlers": {
+              "run": function anonymous(
+) {
+console.log(window.iterator)
+}
+            },
+            "title": "prime_task_answer",
+            "timeout": "17"
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_x top\"\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n  \u003Ch2 class=\"h2_x bottom\"\u003EXXXXXXXXXX\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E ",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Continue →",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": ""
+            },
+            "parameters": {},
+            "messageHandlers": {},
+            "title": "end_mask",
+            "timeout": "50"
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003E${ parameters.target_task }\u003C\u002Fh2\u003E\r\n  \u003Cinput name=\"target_task_answer\" type=\"text\" placeholder=\"значение\" id=\"input_${ parameters.name }\" autofocus\u003E\r\n\u003C\u002Fcenter\u003E",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Продолжить",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": "done"
+            },
+            "parameters": {},
+            "messageHandlers": {
+              "end": function anonymous(
+) {
+window.timeout_message_show = this.data.ended_on == 'timeout'
+}
+            },
+            "title": "target_task_answer",
+            "timeout": "7000",
+            "timeline": []
+          },
+          {
+            "type": "lab.html.Page",
+            "items": [
+              {
+                "required": true,
+                "type": "html",
+                "content": "\u003Ccenter\u003E\r\n  \u003Ch2\u003EВремя вышло!\u003C\u002Fh2\u003E\r\n\u003C\u002Fcenter\u003E",
+                "name": ""
+              }
+            ],
+            "scrollTop": true,
+            "submitButtonText": "Continue →",
+            "submitButtonPosition": "hidden",
+            "files": {},
+            "responses": {
+              "": ""
+            },
+            "parameters": {},
+            "messageHandlers": {
+              "after:prepare": function anonymous(
+) {
+if (window.timeout_message_show)
+  this.options.skip = false
+else
+  this.options.skip = true
+}
+            },
+            "title": "timeout_message",
+            "tardy": true,
             "timeout": "500"
           }
         ]
@@ -206,7 +628,7 @@ const study = lab.util.fromObject({
       "messageHandlers": {
         "end": function anonymous(
 ) {
-alert('this is the end')
+alert('Пожалуйста нажмите на кнопку Download Data (чтобы скачать файл с результатами) и перекиньте скаченный файл мне.')
 }
       },
       "title": "thx",
