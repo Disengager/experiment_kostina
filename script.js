c2817d4ddf4990799366a9396f3e8273f3b170e7
Disengager <disengagerAMV@gmail.com>
2021-12-02 21:34:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ window.biograph_data = this.data;
           "target_task": "12 + 25",
           "prime_task_answer": 12,
           "prime_task_wronganser": 15,
-          "counterbalancing": 1,
+          "counterbalancing": 3,
           "name": "prime1"
         },
         {
@@ -105,7 +105,7 @@ window.biograph_data = this.data;
           "target_task": "17 + 12",
           "prime_task_answer": 9,
           "prime_task_wronganser": 7,
-          "counterbalancing": 3,
+          "counterbalancing": 2,
           "name": "prime2"
         },
         {
@@ -113,7 +113,7 @@ window.biograph_data = this.data;
           "target_task": "19 + 27",
           "prime_task_answer": 10,
           "prime_task_wronganser": 13,
-          "counterbalancing": 2,
+          "counterbalancing": 1,
           "name": "prime3"
         },
         {
@@ -121,22 +121,22 @@ window.biograph_data = this.data;
           "target_task": "15 + 16",
           "prime_task_answer": 5,
           "prime_task_wronganser": 7,
-          "counterbalancing": 3,
+          "counterbalancing": 1,
           "name": "prime4"
         },
         {
           "prime_task": "9 + 2",
           "target_task": "30 + 22",
           "prime_task_answer": 11,
-          "prime_task_wronganser": 9,
-          "counterbalancing": 1,
+          "prime_task_wronganser": 19,
+          "counterbalancing": 3,
           "name": "prime5"
         },
         {
           "prime_task": "5 + 4",
           "target_task": "22 + 51",
           "prime_task_answer": 9,
-          "prime_task_wronganser": 11,
+          "prime_task_wronganser": 1,
           "counterbalancing": 2,
           "name": "prime6"
         },
@@ -144,7 +144,7 @@ window.biograph_data = this.data;
           "prime_task": "7 + 4",
           "target_task": "20 + 48",
           "prime_task_answer": 11,
-          "prime_task_wronganser": 3,
+          "prime_task_wronganser": 13,
           "counterbalancing": 2,
           "name": "prime7"
         },
@@ -177,7 +177,7 @@ window.biograph_data = this.data;
           "target_task": "16 + 50",
           "prime_task_answer": 14,
           "prime_task_wronganser": 15,
-          "counterbalancing": 2,
+          "counterbalancing": 3,
           "name": "prime11"
         },
         {
@@ -185,7 +185,7 @@ window.biograph_data = this.data;
           "target_task": "39 + 48",
           "prime_task_answer": 11,
           "prime_task_wronganser": 12,
-          "counterbalancing": 3,
+          "counterbalancing": 2,
           "name": "prime12"
         },
         {
@@ -193,7 +193,7 @@ window.biograph_data = this.data;
           "target_task": "29 + 40",
           "prime_task_answer": 15,
           "prime_task_wronganser": 13,
-          "counterbalancing": 3,
+          "counterbalancing": 2,
           "name": "prime13"
         },
         {
@@ -201,7 +201,7 @@ window.biograph_data = this.data;
           "target_task": "36 + 58",
           "prime_task_answer": 9,
           "prime_task_wronganser": 8,
-          "counterbalancing": 2,
+          "counterbalancing": 1,
           "name": "prime14"
         },
         {
@@ -209,7 +209,7 @@ window.biograph_data = this.data;
           "target_task": "54 + 38",
           "prime_task_answer": 5,
           "prime_task_wronganser": 6,
-          "counterbalancing": 1,
+          "counterbalancing": 3,
           "name": "prime15"
         },
         {
@@ -217,7 +217,7 @@ window.biograph_data = this.data;
           "target_task": "22 + 76",
           "prime_task_answer": 8,
           "prime_task_wronganser": 9,
-          "counterbalancing": 2,
+          "counterbalancing": 3,
           "name": "prime16"
         },
         {
@@ -233,7 +233,7 @@ window.biograph_data = this.data;
           "target_task": "53 + 19",
           "prime_task_answer": 6,
           "prime_task_wronganser": 8,
-          "counterbalancing": 3,
+          "counterbalancing": 2,
           "name": "prime18"
         },
         {
@@ -241,7 +241,7 @@ window.biograph_data = this.data;
           "target_task": "28 + 22",
           "prime_task_answer": 7,
           "prime_task_wronganser": 5,
-          "counterbalancing": 2,
+          "counterbalancing": 3,
           "name": "prime19"
         },
         {
@@ -249,7 +249,7 @@ window.biograph_data = this.data;
           "target_task": "15 + 58",
           "prime_task_answer": 6,
           "prime_task_wronganser": 9,
-          "counterbalancing": 1,
+          "counterbalancing": 2,
           "name": "prime20"
         },
         {
@@ -257,7 +257,7 @@ window.biograph_data = this.data;
           "target_task": "25 + 53",
           "prime_task_answer": 4,
           "prime_task_wronganser": 8,
-          "counterbalancing": 3,
+          "counterbalancing": 1,
           "name": "prime21"
         },
         {
@@ -331,6 +331,486 @@ window.biograph_data = this.data;
           "prime_task_wronganser": 19,
           "counterbalancing": 2,
           "name": "prime30"
+        },
+        {
+          "prime_task": "5 + 3",
+          "target_task": "46 + 17",
+          "prime_task_answer": 8,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 3,
+          "name": "prime31"
+        },
+        {
+          "prime_task": "4 + 1",
+          "target_task": "56 + 38",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 1,
+          "name": "prime32"
+        },
+        {
+          "prime_task": "1 + 2",
+          "target_task": "35 + 33",
+          "prime_task_answer": 3,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 2,
+          "name": "prime33"
+        },
+        {
+          "prime_task": "8 + 2",
+          "target_task": "46 + 43",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 14,
+          "counterbalancing": 2,
+          "name": "prime34"
+        },
+        {
+          "prime_task": "3 + 6",
+          "target_task": "39 + 60",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 3,
+          "counterbalancing": 3,
+          "name": "prime35"
+        },
+        {
+          "prime_task": "5 + 2",
+          "target_task": "17 + 30",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 2,
+          "counterbalancing": 1,
+          "name": "prime36"
+        },
+        {
+          "prime_task": "2 + 3",
+          "target_task": "36 + 44",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 1,
+          "name": "prime37"
+        },
+        {
+          "prime_task": "5 + 5",
+          "target_task": "13 + 75",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 16,
+          "counterbalancing": 2,
+          "name": "prime38"
+        },
+        {
+          "prime_task": "2 + 2",
+          "target_task": "45 + 47",
+          "prime_task_answer": 4,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 3,
+          "name": "prime39"
+        },
+        {
+          "prime_task": "3 + 6",
+          "target_task": "44 + 53",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 3,
+          "name": "prime40"
+        },
+        {
+          "prime_task": "7 + 9",
+          "target_task": "20 + 32",
+          "prime_task_answer": 16,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 1,
+          "name": "prime41"
+        },
+        {
+          "prime_task": "4 + 3",
+          "target_task": "71 + 14",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime42"
+        },
+        {
+          "prime_task": "6 + 3",
+          "target_task": "36 + 39",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 3,
+          "name": "prime43"
+        },
+        {
+          "prime_task": "8 + 3",
+          "target_task": "46 + 45",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 2,
+          "name": "prime44"
+        },
+        {
+          "prime_task": "1 + 6",
+          "target_task": "46 + 11",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 1,
+          "name": "prime45"
+        },
+        {
+          "prime_task": "1 + 8",
+          "target_task": "12 + 49",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 2,
+          "name": "prime46"
+        },
+        {
+          "prime_task": "7 + 7",
+          "target_task": "59 + 32",
+          "prime_task_answer": 14,
+          "prime_task_wronganser": 16,
+          "counterbalancing": 1,
+          "name": "prime47"
+        },
+        {
+          "prime_task": "8 + 9",
+          "target_task": "49 + 33",
+          "prime_task_answer": 17,
+          "prime_task_wronganser": 18,
+          "counterbalancing": 3,
+          "name": "prime48"
+        },
+        {
+          "prime_task": "5 + 6",
+          "target_task": "64 + 27",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 10,
+          "counterbalancing": 3,
+          "name": "prime49"
+        },
+        {
+          "prime_task": "9 + 5",
+          "target_task": "56 + 29",
+          "prime_task_answer": 14,
+          "prime_task_wronganser": 17,
+          "counterbalancing": 1,
+          "name": "prime50"
+        },
+        {
+          "prime_task": "8 + 2",
+          "target_task": "79 + 12",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 12,
+          "counterbalancing": 2,
+          "name": "prime51"
+        },
+        {
+          "prime_task": "1 + 5",
+          "target_task": "67 + 17",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 3,
+          "name": "prime52"
+        },
+        {
+          "prime_task": "8 + 1",
+          "target_task": "11 + 79",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 1,
+          "name": "prime53"
+        },
+        {
+          "prime_task": "5 + 7",
+          "target_task": "52 + 19",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 2,
+          "name": "prime54"
+        },
+        {
+          "prime_task": "9 + 2",
+          "target_task": "56 + 29",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 18,
+          "counterbalancing": 3,
+          "name": "prime55"
+        },
+        {
+          "prime_task": "8 + 6",
+          "target_task": "44 + 28",
+          "prime_task_answer": 14,
+          "prime_task_wronganser": 19,
+          "counterbalancing": 1,
+          "name": "prime56"
+        },
+        {
+          "prime_task": "7 + 1",
+          "target_task": "66 + 28",
+          "prime_task_answer": 8,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime57"
+        },
+        {
+          "prime_task": "5 + 1",
+          "target_task": "28 + 26",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 1,
+          "name": "prime58"
+        },
+        {
+          "prime_task": "3 + 8",
+          "target_task": "31 + 59",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 17,
+          "counterbalancing": 2,
+          "name": "prime59"
+        },
+        {
+          "prime_task": "1 + 4",
+          "target_task": "17 + 35",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 1,
+          "counterbalancing": 3,
+          "name": "prime60"
+        },
+        {
+          "prime_task": "9 + 1",
+          "target_task": "19 + 15",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 1,
+          "name": "prime61"
+        },
+        {
+          "prime_task": "6 + 4",
+          "target_task": "27 + 57",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 2,
+          "name": "prime62"
+        },
+        {
+          "prime_task": "1 + 1",
+          "target_task": "47 + 46",
+          "prime_task_answer": 2,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 3,
+          "name": "prime63"
+        },
+        {
+          "prime_task": "3 + 1",
+          "target_task": "55 + 18",
+          "prime_task_answer": 4,
+          "prime_task_wronganser": 1,
+          "counterbalancing": 2,
+          "name": "prime64"
+        },
+        {
+          "prime_task": "7 + 9",
+          "target_task": "36 + 29",
+          "prime_task_answer": 16,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 1,
+          "name": "prime65"
+        },
+        {
+          "prime_task": "8 + 5",
+          "target_task": "67 + 16",
+          "prime_task_answer": 13,
+          "prime_task_wronganser": 17,
+          "counterbalancing": 3,
+          "name": "prime66"
+        },
+        {
+          "prime_task": "6 + 4",
+          "target_task": "45 + 46",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 13,
+          "counterbalancing": 3,
+          "name": "prime67"
+        },
+        {
+          "prime_task": "2 + 3",
+          "target_task": "54 + 38",
+          "prime_task_answer": 5,
+          "prime_task_wronganser": 3,
+          "counterbalancing": 2,
+          "name": "prime68"
+        },
+        {
+          "prime_task": "7 + 9",
+          "target_task": "24 + 19",
+          "prime_task_answer": 16,
+          "prime_task_wronganser": 10,
+          "counterbalancing": 1,
+          "name": "prime69"
+        },
+        {
+          "prime_task": "3 + 6",
+          "target_task": "14 + 78",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 2,
+          "name": "prime70"
+        },
+        {
+          "prime_task": "1 + 1",
+          "target_task": "25 + 16",
+          "prime_task_answer": 2,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 3,
+          "name": "prime71"
+        },
+        {
+          "prime_task": "3 + 1",
+          "target_task": "13 + 38",
+          "prime_task_answer": 4,
+          "prime_task_wronganser": 7,
+          "counterbalancing": 1,
+          "name": "prime72"
+        },
+        {
+          "prime_task": "9 + 3",
+          "target_task": "44 + 18",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 16,
+          "counterbalancing": 3,
+          "name": "prime73"
+        },
+        {
+          "prime_task": "6 + 1",
+          "target_task": "41 + 49",
+          "prime_task_answer": 7,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime74"
+        },
+        {
+          "prime_task": "1 + 8",
+          "target_task": "27 + 27",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 2,
+          "counterbalancing": 1,
+          "name": "prime75"
+        },
+        {
+          "prime_task": "2 + 7",
+          "target_task": "18 + 17",
+          "prime_task_answer": 9,
+          "prime_task_wronganser": 5,
+          "counterbalancing": 2,
+          "name": "prime76"
+        },
+        {
+          "prime_task": "6 + 8",
+          "target_task": "39 + 44",
+          "prime_task_answer": 14,
+          "prime_task_wronganser": 19,
+          "counterbalancing": 1,
+          "name": "prime77"
+        },
+        {
+          "prime_task": "2 + 4",
+          "target_task": "49 + 47",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 9,
+          "counterbalancing": 3,
+          "name": "prime78"
+        },
+        {
+          "prime_task": "6 + 7",
+          "target_task": "14 + 67",
+          "prime_task_answer": 13,
+          "prime_task_wronganser": 15,
+          "counterbalancing": 3,
+          "name": "prime79"
+        },
+        {
+          "prime_task": "3 + 8",
+          "target_task": "37 + 54",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 12,
+          "counterbalancing": 1,
+          "name": "prime80"
+        },
+        {
+          "prime_task": "2 + 8",
+          "target_task": "25 + 29",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 17,
+          "counterbalancing": 2,
+          "name": "prime81"
+        },
+        {
+          "prime_task": "3 + 1",
+          "target_task": "18 + 48",
+          "prime_task_answer": 4,
+          "prime_task_wronganser": 8,
+          "counterbalancing": 3,
+          "name": "prime82"
+        },
+        {
+          "prime_task": "8 + 4",
+          "target_task": "57 + 19",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 18,
+          "counterbalancing": 1,
+          "name": "prime83"
+        },
+        {
+          "prime_task": "4 + 6",
+          "target_task": "23 + 69",
+          "prime_task_answer": 10,
+          "prime_task_wronganser": 37,
+          "counterbalancing": 2,
+          "name": "prime84"
+        },
+        {
+          "prime_task": "4 + 7",
+          "target_task": "17 + 37",
+          "prime_task_answer": 11,
+          "prime_task_wronganser": 16,
+          "counterbalancing": 2,
+          "name": "prime85"
+        },
+        {
+          "prime_task": "3 + 9",
+          "target_task": "37 + 19",
+          "prime_task_answer": 12,
+          "prime_task_wronganser": 14,
+          "counterbalancing": 1,
+          "name": "prime86"
+        },
+        {
+          "prime_task": "5 + 1",
+          "target_task": "11 + 19",
+          "prime_task_answer": 6,
+          "prime_task_wronganser": 3,
+          "counterbalancing": 3,
+          "name": "prime87"
+        },
+        {
+          "prime_task": "9 + 6",
+          "target_task": "59 + 32",
+          "prime_task_answer": 15,
+          "prime_task_wronganser": 11,
+          "counterbalancing": 1,
+          "name": "prime88"
+        },
+        {
+          "prime_task": "2 + 1",
+          "target_task": "11 + 29",
+          "prime_task_answer": 3,
+          "prime_task_wronganser": 2,
+          "counterbalancing": 3,
+          "name": "prime89"
+        },
+        {
+          "prime_task": "6 + 2",
+          "target_task": "27 + 55",
+          "prime_task_answer": 8,
+          "prime_task_wronganser": 6,
+          "counterbalancing": 2,
+          "name": "prime90"
         }
       ],
       "sample": {
